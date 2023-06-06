20b68fdedae5feb26d09cf3e75d8b58d38067bfb
Mihovil <airmiha@gmail.com>
2023-06-06 11:39:26 +0200

    Update Typescript config

--- src/views/admin/videos/index.tsx
+++ src/views/admin/videos/index.tsx
@@ -1,37 +1,8 @@
-import { useState } from 'react';
+import { Box, Flex, Text, useColorModeValue } from '@chakra-ui/react';
 
-import {
-  Box,
-  Button,
-  Flex,
-  Skeleton,
-  Stack,
-  Text,
-  useColorModeValue,
-} from '@chakra-ui/react';
-
-import { useVideos, useDeleteVideo } from '@/api/videos';
-
-import ConfirmationModal from '@/components/alerts/ConfirmationModal';
 import Card from '@/components/card/Card';
-import { Video } from '@/components/types';
 
 export default function Videos() {
-  const [editingVideo, setEditingVideo] = useState<Video | null>(null);
-  const [deletingVideo, setDeletingVideo] = useState<number | null>(null);
-
-  const { removeVideo } = useDeleteVideo();
-
-  const handleConfirmDelete = () => {
-    if (deletingVideo) {
-      removeVideo(deletingVideo);
-    }
-
-    setDeletingVideo(null);
-  };
-
-  const { data: videos = [], isLoading } = useVideos();
-
   const textColor = useColorModeValue('secondaryGray.900', 'white');
 
   return (
@@ -51,31 +22,8 @@ export default function Videos() {
           >
             Videos
           </Text>
-          <Button
-            colorScheme="brand"
-            onClick={() =>
-              setEditingVideo({ url: '', maskUrl: '', name: '', metadata: '' })
-            }
-          >
-            Add Video
-          </Button>
         </Flex>
-        {isLoading ? (
-          <Stack padding="16px">
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-            <Skeleton height="20px" />
-          </Stack>
-        ) : null}
       </Card>
-
-      {deletingVideo && (
-        <ConfirmationModal
-          onConfirm={handleConfirmDelete}
-          onClose={() => setDeletingVideo(null)}
-          title="Delete video"
-        />
-      )}
     </Box>
   );
 }
