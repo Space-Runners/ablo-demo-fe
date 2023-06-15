cb080be193390d142053998c824a05d5433fed61
Mihovil <airmiha@gmail.com>
2023-06-15 09:39:23 +0200

    Update API URL

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,5 @@
 const config = {
-  API_URL:
-    import.meta.env.API_URL ||
-    'http://image-generator.eu-central-1.elasticbeanstalk.com',
+  API_URL: import.meta.env.API_URL || 'https://dev.ablo.ai/generate/engines',
   GOOGLE_CLIENT_ID:
     '3985027070-joibmdj5n2l3s5he4c12gt5mme1sp6m7.apps.googleusercontent.com',
 };
