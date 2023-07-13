ca0e8afea3a50273878164bd2237773f4c419de3
Mihovil <airmiha@gmail.com>
2023-07-13 18:21:53 +0200

    Add URL from env

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: import.meta.env.API_URL || 'https://dev.ablo.ai',
+  API_URL: import.meta.env.VITE_API_URL || 'https://dev.ablo.ai',
   GOOGLE_CLIENT_ID:
     '3985027070-joibmdj5n2l3s5he4c12gt5mme1sp6m7.apps.googleusercontent.com',
 };
