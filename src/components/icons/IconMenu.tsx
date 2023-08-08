099b80fb0ce70aae8f3b3358348489fe94078bf7
Mihovil <airmiha@gmail.com>
2023-08-08 15:08:01 +0200

    Design menu and delete option

---
+++ src/components/icons/IconMenu.tsx
@@ -0,0 +1,11 @@
+import { Icon } from "@chakra-ui/react";
+
+const IconMenu = () => (
+  <Icon width="5px" height="16px" viewBox="0 0 5 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="2.38728" cy="2.63006" r="1.6474" fill="#A4A4A4" />
+    <circle cx="2.38728" cy="8.1214" r="1.6474" fill="#A4A4A4" />
+    <circle cx="2.38728" cy="13.6127" r="1.6474" fill="#A4A4A4" />
+  </Icon>
+);
+
+export default IconMenu;
