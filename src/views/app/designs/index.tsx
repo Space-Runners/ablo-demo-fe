e29fc5d0eea6999e544ca14542d14662484f9c2a
Mihovil <airmiha@gmail.com>
2023-08-14 14:33:20 +0200

    Redesign share page

--- src/views/app/designs/index.tsx
+++ src/views/app/designs/index.tsx
@@ -1,5 +1,5 @@
-import { Fragment as F } from "react";
-import { useHistory } from "react-router-dom";
+import { Fragment as F } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import {
   Box,
@@ -8,7 +8,6 @@ import {
   Center,
   Flex,
   HStack,
-  Icon,
   Image,
   Menu,
   MenuButton,
@@ -17,48 +16,19 @@ import {
   Spinner,
   Stack,
   Text,
-} from "@chakra-ui/react";
+} from '@chakra-ui/react';
 
-import Button from "@/components/Button";
-import IconMenu from "@/components/icons/IconMenu";
-import Navbar from "@/components/navbar/Navbar";
+import Button from '@/components/Button';
+import IconCreateNew from '@/components/icons/IconCreateNew';
+import IconMenu from '@/components/icons/IconMenu';
+import Navbar from '@/components/navbar/Navbar';
 
-import { Design } from "@/components/types";
-import { useDesigns, useDeleteDesign } from "@/api/designs";
-import Colors from "@/theme/colors";
-import { timeAgo } from "@/utils/time";
+import { Design } from '@/components/types';
+import { useDesigns, useDeleteDesign } from '@/api/designs';
+import Colors from '@/theme/colors';
+import { timeAgo } from '@/utils/time';
 
 const { abloBlue } = Colors;
-
-const IconCreateNew = ({ color = null, ...rest }) => (
-  <Icon
-    width="17px"
-    height="17px"
-    viewBox="0 0 17 17"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    {...rest}
-  >
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M8.50016 2.12516C4.97935 2.12516 2.12516 4.97935 2.12516 8.50016C2.12516 12.021 4.97935 14.8752 8.50016 14.8752C12.021 14.8752 14.8752 12.021 14.8752 8.50016C14.8752 4.97935 12.021 2.12516 8.50016 2.12516ZM0.708496 8.50016C0.708496 4.19694 4.19694 0.708496 8.50016 0.708496C12.8034 0.708496 16.2918 4.19694 16.2918 8.50016C16.2918 12.8034 12.8034 16.2918 8.50016 16.2918C4.19694 16.2918 0.708496 12.8034 0.708496 8.50016Z"
-      fill={color || "#064AC4"}
-    />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M8.49984 4.9585C8.89104 4.9585 9.20817 5.27563 9.20817 5.66683V11.3335C9.20817 11.7247 8.89104 12.0418 8.49984 12.0418C8.10864 12.0418 7.7915 11.7247 7.7915 11.3335V5.66683C7.7915 5.27563 8.10864 4.9585 8.49984 4.9585Z"
-      fill={color || "#064AC4"}
-    />
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M4.9585 8.49984C4.9585 8.10864 5.27563 7.7915 5.66683 7.7915H11.3335C11.7247 7.7915 12.0418 8.10864 12.0418 8.49984C12.0418 8.89104 11.7247 9.20817 11.3335 9.20817H5.66683C5.27563 9.20817 4.9585 8.89104 4.9585 8.49984Z"
-      fill={color || "#064AC4"}
-    />
-  </Icon>
-);
 
 type DesignsListProps = {
   designs: Design[];
@@ -76,7 +46,7 @@ const DesignsList = ({ designs, onSelectedDesign }: DesignsListProps) => {
         {designs.map((design, index) => {
           const { name, editorState } = design;
           // Find first templateUrl
-          let templateUrl = "";
+          let templateUrl = '';
           for (const side in editorState) {
             if (editorState[side].templateUrl) {
               templateUrl = editorState[side].templateUrl;
@@ -96,7 +66,7 @@ const DesignsList = ({ designs, onSelectedDesign }: DesignsListProps) => {
                 <Center cursor="pointer" h="172px" onClick={() => onSelectedDesign(design)}>
                   <Image objectFit="contain" src={templateUrl} alt={name} h={155} px={1} py={4} />
                 </Center>
-                <Stack m={0} p={3} spacing={0.5} bg={"white"} h="57px" borderBottomRadius={10}>
+                <Stack m={0} p={3} spacing={0.5} bg={'white'} h="57px" borderBottomRadius={10}>
                   <Flex justify="space-between" position="relative">
                     <Text fontSize="13px" fontWeight={500}>
                       {name}
@@ -112,16 +82,16 @@ const DesignsList = ({ designs, onSelectedDesign }: DesignsListProps) => {
                         onClick={(e) => e.stopPropagation()}
                         p={0}
                         _hover={{
-                          border: "none",
-                          boxShadow: "none",
+                          border: 'none',
+                          boxShadow: 'none',
                         }}
                         _focus={{
-                          border: "none",
-                          boxShadow: "none",
+                          border: 'none',
+                          boxShadow: 'none',
                         }}
                         _active={{
-                          border: "none",
-                          boxShadow: "none",
+                          border: 'none',
+                          boxShadow: 'none',
                         }}
                       />
                       <MenuList border="none" borderRadius="11px" minWidth="none" p="0" w="124px">
@@ -135,10 +105,10 @@ const DesignsList = ({ designs, onSelectedDesign }: DesignsListProps) => {
                           }}
                           padding="8px 18px"
                           _focus={{
-                            bg: "#F4F4F4",
+                            bg: '#F4F4F4',
                           }}
                           _active={{
-                            bg: "none",
+                            bg: 'none',
                           }}
                         >
                           Delete
@@ -169,7 +139,7 @@ export default function DesignsPage() {
   };
 
   const handleGoToEditor = () => {
-    history.push("/app/editor");
+    history.push('/app/editor');
   };
 
   return (
--- src/views/app/order-or-share/index.tsx
+++ src/views/app/order-or-share/index.tsx
@@ -5,21 +5,31 @@ import {
   Spinner,
   Flex,
   HStack,
-  Icon,
   Image,
   Text,
+  VStack,
 } from '@chakra-ui/react';
 
 import { useLocation, useHistory } from 'react-router-dom';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { Pagination } from 'swiper';
+
 import { useDesign } from '@/api/designs';
 
 import Button from '@/components/Button';
+import IconBack from '@/components/icons/IconBack';
+import IconCreateNew from '@/components/icons/IconCreateNew';
+import IconShare from '@/components/icons/IconShare';
 import Navbar from '@/components/navbar/Navbar';
+import { AiImage, CanvasState } from '@/components/types';
 import Colors from '@/theme/colors';
 
 import { IconInstagram, IconTikTok, IconFacebook } from './Icons';
-import { useState } from 'react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 function getImgUrl(name) {
   return new URL(`./images/${name}.webp`, import.meta.url).href;
@@ -29,89 +39,56 @@ const { abloBlue } = Colors;
 
 const SHARE_OPTIONS = [
   {
-    name: 'Instagram',
-    icon: <IconInstagram />,
+    name: 'getLink',
+    icon: <IconShare h="32px" w="32px" />,
+    title: 'Get a link',
   },
   {
     name: 'TikTok',
     icon: <IconTikTok />,
+    title: 'Share on TikTok',
   },
+  {
+    name: 'Instagram',
+    icon: <IconInstagram />,
+    title: 'Share on Instagram',
+  },
+
   {
     name: 'Facebook',
     icon: <IconFacebook />,
+    title: 'Share on Facebook',
   },
 ];
 
-const IconChangeOrientation = () => (
-  <Icon
-    width="22px"
-    height="22px"
-    viewBox="0 0 22 22"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g clipPath="url(#clip0_2455_8217)">
-      <path
-        d="M15.4999 13.9956C17.6725 13.3494 19.0999 12.2496 19.0999 11.0004C19.0999 9.01139 15.4729 7.40039 10.9999 7.40039C6.5269 7.40039 2.8999 9.01139 2.8999 11.0004C2.8999 12.9894 6.5269 14.6004 10.9999 14.6004"
-        stroke="white"
-        strokeWidth="0.9"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M8.29993 11.9004L10.9999 14.6004L8.29993 17.3004"
-        stroke="white"
-        strokeWidth="0.9"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_2455_8217">
-        <rect width="21.6" height="21.6" fill="white" transform="translate(0.199951 0.200195)" />
-      </clipPath>
-    </defs>
-  </Icon>
-);
+const SLIDES = [
+  {
+    side: 'front',
+    background: true,
+  },
+  {
+    side: 'front',
+    background: false,
+  },
+  { side: 'back', background: true },
+  { side: 'back', background: false },
+];
 
-const IconCopy = () => (
-  <Icon
-    width="25px"
-    height="24px"
-    viewBox="0 0 25 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <g clipPath="url(#clip0_1948_36883)">
-      <path
-        d="M9.5 15L15.5 9"
-        stroke="#064AC4"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M11.5 6.00031L11.963 5.46431C12.9008 4.52663 14.1727 3.99991 15.4989 4C16.825 4.00009 18.0968 4.527 19.0345 5.46481C19.9722 6.40261 20.4989 7.6745 20.4988 9.00066C20.4987 10.3268 19.9718 11.5986 19.034 12.5363L18.5 13.0003"
-        stroke="#064AC4"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M13.5001 18L13.1031 18.534C12.1544 19.4722 10.8739 19.9984 9.53964 19.9984C8.20535 19.9984 6.92489 19.4722 5.97614 18.534C5.5085 18.0716 5.13724 17.521 4.88385 16.9141C4.63047 16.3073 4.5 15.6561 4.5 14.9985C4.5 14.3408 4.63047 13.6897 4.88385 13.0829C5.13724 12.476 5.5085 11.9254 5.97614 11.463L6.50014 11"
-        stroke="#064AC4"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </g>
-    <defs>
-      <clipPath id="clip0_1948_36883">
-        <rect width="24" height="24" fill="white" transform="translate(0.5)" />
-      </clipPath>
-    </defs>
-  </Icon>
-);
+const getAiImageForSide = (side: CanvasState): AiImage => {
+  const { canvas } = side;
+
+  if (!canvas) {
+    return null;
+  }
+
+  const canvasState = JSON.parse(canvas);
+
+  if (!canvasState._objects) {
+    return null;
+  }
+
+  return canvasState._objects.find(({ aiImage }) => !!aiImage)?.aiImage;
+};
 
 export default function OrderOrShare() {
   const { search } = useLocation();
@@ -123,13 +100,9 @@ export default function OrderOrShare() {
 
   const { front, back } = design?.editorState || {};
 
-  const [selectedSide, setSelectedSide] = useState('front');
-
   const history = useHistory();
 
-  const style = 'kidult';
-
-  console.log('Editor state', front, back, selectedSide);
+  const handleGoToDesigns = () => history.push('/app/designs');
 
   return (
     <Box>
@@ -140,7 +113,26 @@ export default function OrderOrShare() {
           </Text>
         }
         onBack={() => history.goBack()}
-        onNext={() => history.push('/app/designs')}
+        onNext={handleGoToDesigns}
+        rightSideContent={
+          <HStack>
+            <Button
+              h="40px"
+              icon={<IconBack />}
+              onClick={() => history.goBack()}
+              outlined
+              textTransform="none"
+              title="Back To Design"
+            />
+            <Button
+              h="40px"
+              icon={<IconCreateNew color="#FFFFFF" />}
+              onClick={handleGoToDesigns}
+              textTransform="none"
+              title="Create New"
+            />
+          </HStack>
+        }
         title="Share"
       />
       {isLoading ? (
@@ -148,75 +140,90 @@ export default function OrderOrShare() {
           <Spinner thickness="1px" speed="0.65s" emptyColor="gray" size="md" />
         </Center>
       ) : (
-        <Box bg="#FFFFFF" h="100%" overflow="auto" w="100%" padding="10px 0">
+        <Flex
+          bg="#FFFFFF"
+          direction={{ base: 'column', md: 'row' }}
+          h={{ base: 'calc(100vh - 121px)', md: 'calc(100vh - 65px)' }}
+          overflow="auto"
+          w="100%"
+        >
           <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            position="relative"
-            w="100%"
+            bg={{ base: 'transparent', md: '#CFD3DE' }}
+            h={{ base: '300px', md: '100%' }}
+            p={{ base: 0, md: '16px' }}
+            flex={1}
           >
-            {front?.templateUrl && back?.templateUrl ? (
-              <Flex
-                align="center"
-                as="button"
-                bg="#000000"
-                borderRadius={0}
-                h="36px"
-                justify="center"
-                onClick={() => setSelectedSide(selectedSide === 'front' ? 'back' : 'front')}
-                position="absolute"
-                left={0}
-                top={0}
-                w="36px"
-              >
-                <IconChangeOrientation />
-              </Flex>
-            ) : null}
-            {style ? <Image w="100%" src={getImgUrl(`${style}`)} alt={style} /> : null}
-            {style ? (
-              <Image
-                src={(selectedSide === 'front' ? front : back)?.templateUrl}
-                margin="0 auto"
-                position="absolute"
-                width={250}
-              />
-            ) : null}
+            <Box
+              display="flex"
+              alignItems="center"
+              bg="#FFFFFF"
+              borderRadius={{ base: 0, md: '10px' }}
+              h="100%"
+              justifyContent="center"
+              position="relative"
+              w="100%"
+            >
+              <Box h="100%" w={{ base: '100%', md: '600px' }}>
+                <Swiper pagination modules={[Pagination]} className="mySwiper">
+                  {SLIDES.map((slide, index) => {
+                    const { side, background } = slide;
+
+                    const stateFoSide = side === 'front' ? front : back;
+
+                    const aiImage = getAiImageForSide(stateFoSide);
+
+                    const style = aiImage ? aiImage.options.style : 'kidult';
+
+                    return (
+                      <SwiperSlide key={index}>
+                        <Box position="relative">
+                          {background ? (
+                            <Image
+                              h={{ base: 350, md: 'auto' }}
+                              w={{ base: '100%', md: '100%' }}
+                              src={getImgUrl(`${style}`)}
+                              alt={style}
+                            />
+                          ) : null}
+                          <Image
+                            src={stateFoSide?.templateUrl}
+                            margin="0 auto"
+                            left={0}
+                            right={0}
+                            top={50}
+                            position="absolute"
+                            w={{ base: 250, md: 500 }}
+                          />
+                        </Box>
+                      </SwiperSlide>
+                    );
+                  })}
+                </Swiper>
+              </Box>
+            </Box>
           </Box>
-          <Box p="0 10px">
-            <Button icon={<IconCopy />} mb="16px" mt="16px" title="Copy share link" w="100%" />
-            <Text mb="24px" textAlign="center">
+          <Box p={{ base: '20px', md: '45px 20px' }} w={{ base: '100%', md: '328px' }}>
+            <Flex mb={{ base: '24px', md: '48px' }}>
+              <IconShare />
+              <Text as="b" fontSize="18px" ml="20px">
+                Share your work
+              </Text>
+            </Flex>
+            <Text as="b" color="#8D8D8D" fontSize="sm">
               Share on your socials
             </Text>
-            {SHARE_OPTIONS.map(({ name, icon }, index) => (
-              <Flex
-                align="center"
-                borderTop={index === 0 ? 'none' : '1px solid #D4D4D3'}
-                justify="space-between"
-                key={index}
-                padding="13px 0"
-                w="100%"
-              >
-                <HStack>
+            <VStack align="flex-start" mt="28px" spacing="20px">
+              {SHARE_OPTIONS.map(({ icon, title }, index) => (
+                <ChakraButton bg="transparent" key={index} padding={0}>
                   {icon}
-                  <Text fontSize="sm" ml="12px">
-                    {name}
+                  <Text as="b" fontSize="sm" ml="12px">
+                    {title}
                   </Text>
-                </HStack>
-                <ChakraButton
-                  bg="transparent"
-                  color={abloBlue}
-                  fontSize="xs"
-                  fontWeight={600}
-                  padding={0}
-                  textTransform="uppercase"
-                >
-                  Share
                 </ChakraButton>
-              </Flex>
-            ))}
+              ))}
+            </VStack>
           </Box>
-        </Box>
+        </Flex>
       )}
     </Box>
   );
