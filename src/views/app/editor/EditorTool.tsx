fa9b6f19d1ac895d625033dc3f22a8b1a573a0cb
Mihovil <airmiha@gmail.com>
2023-08-22 23:39:53 +0200

    Add max text offset

--- src/views/app/editor/EditorTool.tsx
+++ src/views/app/editor/EditorTool.tsx
@@ -234,10 +234,22 @@ export default function ImageEditorTool({ design, onDesignChange, onSave }: Imag
 
     const aiImage = canvas.current._objects.find(({ aiImage }) => aiImage);
 
+    console.log(
+      'Add text',
+      aiImage.aCoords.tl.y,
+      aiImage.height,
+      aiImage.aCoords.tl.y + aiImage.height,
+      height
+    );
+
+    const maxTextOffset = height * 3 - 20;
+
     const textObject = {
       ...defaultProps,
       left: (width * 3) / 2,
-      top: aiImage ? aiImage.aCoords.tl.y + aiImage.height : (height * 3) / 2 - 20,
+      top: aiImage
+        ? Math.min(aiImage.aCoords.tl.y + aiImage.height, maxTextOffset)
+        : (height * 3) / 2 - 20,
     };
 
     console.log('Text object', textObject);
