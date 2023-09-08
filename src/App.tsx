9f20bb8bddde9da4d6639f169c9be6da64f7034f
kvarela <karim@metalink.com>
2023-09-08 14:48:43 -0700

    Added queryClient fix to <App />

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { PasswordWallModal } from './components/modals/PasswordWallModal';
 import { StorageKeys } from './constants';
+import { isAxiosError } from 'axios';
 
 const { GOOGLE_CLIENT_ID } = Config;
 
@@ -21,10 +22,27 @@ export const App: React.FC = () => {
     localStorage.getItem(StorageKeys.HAS_ACCESS) === 'true'
   );
 
+  const MAX_RETRIES = 6;
+  const HTTP_STATUS_TO_NOT_RETRY = [400, 401, 403, 404, 409];
+
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        refetchOnWindowFocus: false,
+        retry: (failureCount, error) => {
+          if (failureCount > MAX_RETRIES) {
+            return false;
+          }
+
+          if (
+            isAxiosError(error) &&
+            HTTP_STATUS_TO_NOT_RETRY.includes(error.response?.status ?? 0)
+          ) {
+            console.log(`Aborting retry due to ${error.response?.status} status`);
+            return false;
+          }
+
+          return true;
+        },
       },
     },
   });
