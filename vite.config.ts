f30a8ade8ad688fe4fdb373bd66fe2b94cb6533b
Mihovil <airmiha@gmail.com>
2023-09-27 15:58:06 +0200

    Add library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path, { resolve } from 'path';
-
-import dts from 'vite-plugin-dts';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.tsx'),
-      formats: ['es'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-    },
-  },
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') },
   },
-  plugins: [react(), dts()],
+  plugins: [react()],
 });
