83e87a1b91aed8e4a915c4e85f462d0ed9b9f36a
Mihovil <airmiha@gmail.com>
2023-10-13 21:30:48 +0200

    Remove opacity picker

--- src/lib/components/modals/ColorPickerModal.tsx
+++ src/lib/components/modals/ColorPickerModal.tsx
@@ -1,15 +1,6 @@
-import {
-  Box,
-  Button,
-  Flex,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalBody,
-  Text,
-} from '@chakra-ui/react';
+import { Button, Flex, Modal, ModalOverlay, ModalContent, ModalBody, Text } from '@chakra-ui/react';
 
-import { AlphaPicker, SketchPicker } from 'react-color';
+import { SketchPicker } from 'react-color';
 
 import IconCloseModalSmall from '../icons/IconCloseModalSmall';
 
@@ -17,21 +8,13 @@ type Props = {
   onClose: () => void;
   onSelectedColor: (color: string) => void;
   selectedColor: string;
-  onSelectedOpacity: (opacity: number) => void;
-  selectedOpacity: number;
 };
 
-const ColorPickerModal = ({
-  onClose,
-  onSelectedColor,
-  selectedColor,
-  selectedOpacity,
-  onSelectedOpacity,
-}: Props) => {
+const ColorPickerModal = ({ onClose, onSelectedColor, selectedColor }: Props) => {
   return (
     <Modal isOpen={true} onClose={onClose} motionPreset="slideInBottom">
       <ModalOverlay />
-      <ModalContent alignSelf="center" borderRadius="10px" h="500px" w="328px">
+      <ModalContent alignSelf="center" borderRadius="10px" h="450px" w="328px">
         <ModalBody padding="15px 12px">
           <Flex alignItems="center" justifyContent="space-between" mb="20px">
             <Text as="b" fontSize="sm">
