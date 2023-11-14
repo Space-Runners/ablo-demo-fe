a9dabdff005f19981e945951afa78dde83ab2c40
Mihovil <airmiha@gmail.com>
2023-11-14 16:00:51 +0100

    Add Enter handler on all auth forms

--- src/views/auth/components/ButtonCTA.tsx
+++ src/views/auth/components/ButtonCTA.tsx
@@ -1,18 +1,39 @@
-import { Button } from "@chakra-ui/react";
+import { useEffect } from 'react';
+import { Button } from '@chakra-ui/react';
 
-const ButtonCTA = ({ title, ...rest }) => (
-  <Button
-    bg="#0D6EFD"
-    borderRadius="4px"
-    color="#FFFFFF"
-    fontWeight={400}
-    h="36px"
-    padding="6px 0"
-    w="100%"
-    {...rest}
-  >
-    {title}
-  </Button>
-);
+const ButtonCTA = ({ onClick, title, ...rest }) => {
+  useEffect(() => {
+    const keyDownHandler = (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+
+        // 👇️ your logic here
+        onClick();
+      }
+    };
+
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, [onClick]);
+
+  return (
+    <Button
+      bg="#0D6EFD"
+      borderRadius="4px"
+      color="#FFFFFF"
+      fontWeight={400}
+      h="36px"
+      padding="6px 0"
+      w="100%"
+      onClick={onClick}
+      {...rest}
+    >
+      {title}
+    </Button>
+  );
+};
 
 export default ButtonCTA;
